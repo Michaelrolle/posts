3591e181e2ec279c3e66034f2db837147d1ed64e
Michael <michael.rolle@hotmail.com>
2020-07-22 14:56:36 +0200

    init

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Post text</th>
+        <th>Created date</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Something</td>
+        <td>Longer text</td>
+        <td>2020-01-23</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Something 2</td>
+        <td>Longer text</td>
+        <td>2020-01-24</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Something 3</td>
+        <td>Longer text</td>
+        <td>2020-01-26</td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
